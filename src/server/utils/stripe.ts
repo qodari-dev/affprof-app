0c26bfda3eacf91d785ad6c71333cf47f2b8729a
Carlos Jose Sanchez <carlosjosesancheze@gmail.com>
2026-04-16 19:32:17 -0400

    stripe issue

--- src/server/utils/stripe.ts
+++ src/server/utils/stripe.ts
@@ -1,6 +1,19 @@
 import Stripe from 'stripe';
 import { env } from '@/env';
 
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  typescript: true,
+let _stripe: Stripe | null = null;
+
+export function getStripe(): Stripe {
+  if (!_stripe) {
+    _stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+      typescript: true,
+    });
+  }
+  return _stripe;
+}
+
+export const stripe = new Proxy({} as Stripe, {
+  get(_target, prop) {
+    return getStripe()[prop as keyof Stripe];
+  },
 });
